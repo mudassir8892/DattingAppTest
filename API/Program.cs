04323083bcf0dcea13e462c3c2f9943ce9ef29f1
mudassir <syedali.mudassir8892@gmail.com>
2024-05-03 03:17:58 +0500

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,49 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Routing.Tree;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+// builder.Services.AddDbContext<DataContext>(opt =>
+// {
+//     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
 
-builder.Services.AddCors();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+// builder.Services.AddCors();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// // builder.Services.AddEndpointsApiExplorer();
+// // builder.Services.AddSwaggerGen();
 
+// builder.Services.AddScoped<ITokenService, TokenService>();
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(Options =>
+//     {
+//         Options.TokenValidationParameters = new TokenValidationParameters
+//         {
+//             ValidateIssuerSigningKey = true,
+//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+//             ValidateIssuer = false,
+//             ValidateAudience = false
+//         };
+//     });
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder
+    .AllowAnyHeader() // Allow any HTTP headers in the request
+    .AllowAnyMethod() // Allow any HTTP method (GET, POST, PUT, DELETE, etc.)
+    .WithOrigins("http://localhost:4200")); // Allow requests from the specified origin
 
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
@@ -29,6 +54,7 @@ app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("ht
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
